aa4dd50bbc7b38086794d83bdb1d3ffece2ded69
socket772 <paolotto.ghilardi@gmail.com>
2024-04-01 19:36:25 +0200

    Aggiunta opzioni per decidere se sovrascrivere file, aggiunta descrizione in --help

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,28 @@ static DEFAULT_INPUT:&str = "./input";
 static DEFAULT_OUTPUT:&str = "./output";
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, about = "Utility scritta in rust per convertire in massa file multimediali in mp3 usando il multi threading")]
 struct Args {
+
 	// Numero dei thread
-	#[arg(short, long, default_value_t = num_cpus::get())]
+	#[arg(short, long, default_value_t = num_cpus::get(), help="Definisci il numero di thread (file convertiti contemporaneamente)")]
 	threadcount: usize,
 
 	// cartella input
-	#[arg(short, long, default_value = DEFAULT_INPUT)]
+	#[arg(short, long, default_value = DEFAULT_INPUT, help="Definisci la cartella di input")]
 	input: String,
 
 	// cartella output
-	#[arg(short, long, default_value = DEFAULT_OUTPUT)]
+	#[arg(short, long, default_value = DEFAULT_OUTPUT, help="Definisci la cartella di output")]
 	output: String,
 
 	// specifica la posizione di ffmpeg
-	#[arg(short, long, default_value = "ffmpeg")]
+	#[arg(short, long, default_value = "ffmpeg", help="Definisci il percorso di ffmpeg (necessario se in Windows)")]
 	program: String,
+
+	// abilita la sovrascrittura dei file
+	#[clap(short, long, help="Definisci se sovrascrivere i file già esistenti")]
+	sovrascrivi: bool,
 }
 
 // Creo una struct per poter condividere le informazioni con i miei thread
@@ -41,7 +46,9 @@ struct Canzoni {
 	// Cartella di output
 	output_folder: String,
 	// Percorso di ffmpeg
-	program: String
+	program: String,
+	// Sovrascrittura abilitata
+	sovrascrivi: bool
 }
 
 fn main() {
@@ -93,7 +100,8 @@ fn main() {
 		posizione: 0,
 		input_folder: input_folder_arg,
 		output_folder: output_folder_arg,
-		program: program
+		program: program,
+		sovrascrivi: args.sovrascrivi
 	};
 
 	// Instanzio il lucchetto Mutex che usero per accedere ai dati condivisi
@@ -121,6 +129,7 @@ fn main() {
 				let input_folder = dati_condivisi.input_folder.clone();
 				let output_folder = dati_condivisi.output_folder.clone();
 				let program_temp = dati_condivisi.program.clone();
+				let sovrascrivi_temp = dati_condivisi.sovrascrivi;
 
 				
 				// Controllo se ci sono altre canzoni da convertire
@@ -142,7 +151,13 @@ fn main() {
 				let canzone_input_path = format!("{}/{}", input_folder, nome_canzone);
 				let canzone_output_path = format!("{}/{}.mp3", output_folder, nome_canzone);
 
-				let argomenti = ["-loglevel", "panic", "-nostats", "-i", canzone_input_path.as_str(),"-c:v", "copy", "-c:a", "libmp3lame", "-q:a", "4", "-threads", "4", canzone_output_path.as_str()];
+				let mut sovrascrivi_arg = "-n";
+
+				if sovrascrivi_temp {
+					sovrascrivi_arg = "-y";
+				}
+
+				let argomenti = [sovrascrivi_arg, "-loglevel", "panic", "-nostats", "-i", canzone_input_path.as_str(),"-c:v", "copy", "-c:a", "libmp3lame", "-q:a", "4", "-threads", "4", canzone_output_path.as_str()];
 				
 				let command = Command::new(program_temp).args(argomenti).spawn().unwrap().wait();
 				if command.is_err(){
